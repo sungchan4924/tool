824aa6640a4eb93e3e23738c008fde93f52672c0
sungchan4924 <chan4924@gmail.com>
2026-07-05 21:45:33 +0900

    Add files via upload

---
+++ index.html/6index.html.js
@@ -0,0 +1,4 @@
+const CACHE='h6300-pro-v2';
+const FILES=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
